f08ae7eda42a38865512e9a4a0073ac7d7a2f1a0
TestardR <romain.rtestard@gmail.com>
2020-12-20 13:40:26 +0100

    Revert "I have added auth"
    This reverts commit d15b01a9698eb814cbfee14c0514cbd205331d62.

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -128,5 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-AUTH_USER_MODEL = 'authentication.User'
