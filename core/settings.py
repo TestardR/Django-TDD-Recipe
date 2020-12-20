2ea3e4bb20ff18ef15cb714a5f755617b9c53c54
TestardR <romain.rtestard@gmail.com>
2020-12-20 10:11:26 +0100

    I have moved secret_key to env

--- core/settings.py
+++ core/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f71nxijg_#2k&bi39k3i85f!m$x0fs834o4+#vev6lv!fxf-jl'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
