ac371950e7143c77c790de7b67bcbad081db4722
Romain Testard <romain.testard@bluenove.com>
2020-12-27 17:49:37 +0100

    I have finished implementing and testing admin user

--- authentication/tests/test_admin.py
+++ authentication/tests/test_admin.py
@@ -13,9 +13,35 @@ def auto_login_superuser(db, admin_client):
                        password=user.password)
 
 
+@pytest.fixture
+def create_superuser(db):
+    def make_user():
+        return get_user_model().objects.create_superuser(
+            'test@test.com',
+            'test123'
+        )
+    return make_user
+
+
 def test_users_listed(auto_login_superuser, admin_client):
     """ Test that users are listed on user page """
     url = reverse('admin:authentication_user_changelist')
+    response = admin_client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_user_change_page(create_superuser, admin_client):
+    """ Test that the user edit page works """
+    user = create_superuser()
+    url = reverse('admin:authentication_user_change', args=[user.id])
+    response = admin_client.get(url)
+
+    assert response.status_code == 200
+
+def test_create_user_page(create_superuser, admin_client):
+    """ Test that the create user page works """
+    url = reverse('admin:authentication_user_add')
     response = admin_client.get(url)
 
     assert response.status_code == 200
